183542cc05b340f9876e6f934d7c057b77d02d68
Christopher Hilgner <hilgi45@gmail.com>
2023-09-20 14:15:08 +0200

    Initial Commit

---
+++ finance-app-frontend/src/components/NavBar.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { text } from "@sveltejs/kit";
+    import NavBarButton from "./NavBarButton.svelte";
+</script>
+
+<div class="w-screen bg-slate-500 py-5 sticky top-0 left-0 flex flex-row space-x-2">
+    <NavBarButton text={"Übersicht"} func={() => {console.log("Test")}} />
+</div>
---
+++ finance-app-frontend/src/components/NavBarButton.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let text: string = "";
+    export let func = () => {};
+</script>
+
+<button on:click={func} class="font-bold bg-slate-600 text-white py-2 px-3 rounded hover:bg-slate-700">
+    {text}
+</button>
